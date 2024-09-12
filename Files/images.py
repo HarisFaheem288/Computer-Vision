3d438e9257031839a8da6bfffda4b58bbf9d1149
Muhammad Haris <harisfaheem288@gmail.com>
2024-09-12 20:25:44 +0500

    Update images.py

--- Files/images.py
+++ Files/images.py
@@ -1,1 +1,13 @@
+import cv2
+def load_and_show_image(image_path):
+    image = cv2.imread(image_path)
+    if image is not None:
+        cv2.imshow('Loaded Image', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print(f"Error: Could not load image at {image_path}")
+load_and_show_image('uber.jpg')
+load_and_show_image('a.jpg')
+
 
